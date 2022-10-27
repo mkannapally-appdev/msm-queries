3fb746f19aa00354b231e15912518cfa6dff348e
mkannapally <akannapa@chicagobooth.edu>
2022-10-27 21:12:02 +0000

    eldest url created!

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,10 +1,12 @@
 class DirectorsController < ApplicationController
 
   def index
-
     @list_of_directors = Director.all
-    
     render({:template => "director_templates/index.html.erb"})
   end
 
+  def wisest
+    @oldest_director = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
+    render({:template => "director_templates/wisest.html.erb"})
+  end
 end
