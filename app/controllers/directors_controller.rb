99b6f5e3f0e052d6cfed57896085a9cfaeecc2bb
mkannapally <akannapa@chicagobooth.edu>
2022-10-28 05:16:20 +0000

    youngest & eldest working!

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,4 +9,19 @@ class DirectorsController < ApplicationController
     @oldest_director = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
     render({:template => "director_templates/wisest.html.erb"})
   end
+
+  def youngest
+    @youngest_director = Director.where.not({:dob => nil}).order({:dob => :desc}).at(0)
+    render({:template => "director_templates/youngest.html.erb"})
+  end
+
+
+  def director_details
+
+    @the_id = params.fetch("an_id")
+    Director.all.where({:id=> @the_id}).at(0)
+    
+    render({:template => "director_templates/details.html.erb"})
+  
+  end
 end
